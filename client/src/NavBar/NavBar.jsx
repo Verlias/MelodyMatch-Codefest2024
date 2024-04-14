33157b4471b0c75ee8deaa3676baecda04ceb883
Logan <loganvoravong@gmail.com>
2024-04-14 14:40:52 -0400

    Setup Profile Page

--- client/src/NavBar/NavBar.jsx
+++ client/src/NavBar/NavBar.jsx
@@ -15,7 +15,7 @@ function NavBar(){
                         <Link className={styles.Links} to="/About">Who we Are</Link>
                     </li>
                     <li>
-                        <Link className={styles.Links} to="/Contact">Contact</Link>
+                        <Link className={styles.Links} to="/Profile">My Profile</Link>
                     </li>
                     <li>
                         <Link className={styles.LoginLink} to="/Login">Login/Register</Link>
