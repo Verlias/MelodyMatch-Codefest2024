1c49cfe809b2bbf1b83fe0bd376a40da52225052
GeorgeEllis123 <70286749+GeorgeEllis123@users.noreply.github.com>
2024-04-19 10:54:40 -0400

    Added script upload

--- client/src/HomePage/Home.jsx
+++ client/src/HomePage/Home.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import NavBar from "../NavBar/NavBar.jsx";
 import styles from "./Home.module.css";
 import neuralnetwork from "../assets/neural.png";
 
 function Home(){
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = "src/scripts/mapscript.js";
+        script.async = true;
+        document.body.appendChild(script);
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+    
     return(
         <>
             <NavBar />
