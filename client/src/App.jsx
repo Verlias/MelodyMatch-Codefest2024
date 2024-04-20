8729dc04c5eb3b2c18e1fd9eb52fc82b4373df51
Leo Thomas <lmt89@drexel.edu>
2024-04-19 22:09:42 -0400

    profile page 1st commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './HomePage/Home';
 import About from './AboutPage/About';
 import ChatPage from './chat-page/chat-page'
+import Profile from './ProfilePage/Profile'
 
 
 function App() {
@@ -16,6 +17,8 @@ function App() {
             <Route path="/Home" element={<Home />}/>
             <Route path="/About" element={<About />}/>
             <Route path="/Chatbot" element={<ChatPage />}/>
+            <Route path="/Profile" element={<Profile />}/>
+            
         </Routes>
       </Router>
     </>
