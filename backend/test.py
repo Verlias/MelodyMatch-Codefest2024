17fd577e6e592a28385e9b1b510eb07825e60c9b
Logan <loganvoravong@gmail.com>
2024-04-20 19:37:39 -0400

    .

--- backend/test.py
+++ backend/test.py
@@ -1,1 +1,6 @@
-print("hello world")
+def slice(link):
+    return link.split('/')[-1]
+
+if __name__ == "__main__":
+    id = slice('https://open.spotify.com/track/2hgzdQdnfWwtdpZbhZlV72?si=d8e38947fb314a29')
+    print(id)
