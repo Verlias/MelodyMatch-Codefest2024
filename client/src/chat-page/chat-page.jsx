ef157bc1f84c0f4ca199cb907c7aa1f25e67d6d2
Logan <loganvoravong@gmail.com>
2024-04-20 16:43:15 -0400

    Added Changes to ChatBox

--- client/src/chat-page/chat-page.jsx
+++ client/src/chat-page/chat-page.jsx
@@ -13,12 +13,12 @@ function ChatPage() {
     }, []);
 
   const [activeButton, setActiveButton] = useState('CombinedAlgo');
+  const [inputValue, setInputValue] = useState('');
 
   const handleButtonClick = (buttonName) => {
     setActiveButton(buttonName);
+    console.log(`Clicked ${buttonName}`);
   };
-
-  const [inputValue, setInputValue] = useState('');
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
@@ -26,7 +26,7 @@ function ChatPage() {
 
   const handleSubmit = () => {
     // Handle submission logic here, such as sending the input value to a backend or performing some action
-    console.log('Submitted input:', inputValue);
+    console.log(`Clicked ${activeButton} and submitted input: ${inputValue}`);
     // Clear the input field after submission
     setInputValue('');
   };
@@ -38,12 +38,12 @@ function ChatPage() {
   };
 
   return (
-      <>
-          <canvas id="canvas" style={{ position: "absolute", left: "50%", top: "25%", transform: "translate(-50%, 0)", zIndex: 999 }}></canvas>
+    <>
+      <canvas id="canvas" style={{ position: "absolute", left: "50%", top: "25%", transform: "translate(-50%, 0)", zIndex: 999 }}></canvas>
       <section className={styles.ChatbotSection}>
         <div className={styles.HeadlineSection}>
           <h1 className={styles.ChatHeadline}>Melody Match</h1>
-          <h3 className={styles.HeadlineDescription}>Your everyday AI music reccomender system</h3>
+          <h3 className={styles.HeadlineDescription}>Your everyday AI music recommender system</h3>
         </div>
         <div className={styles.HeaderOptions}>
           <p className={styles.OptionHeadline}>Choose your groove style</p>
@@ -60,23 +60,16 @@ function ChatPage() {
             >
               Balanced
             </button>
-            <button
-              className={`${styles.FeatureAlgos} ${activeButton === 'AudioFiltering' ? styles.Active : ''}`}
-              onClick={() => handleButtonClick('AudioFiltering')}
-            >
-              Audio Filtering
-            </button>
           </div>
         </div>
         <div className={styles.InputSection}>
-        <input
+          <input
             type="text"
             placeholder="Input your song name"
             value={inputValue}
             onChange={handleInputChange}
             onKeyPress={handleKeyPress}
           />
-          <button onClick={handleSubmit}>Submit</button>
         </div>
       </section>
     </>
