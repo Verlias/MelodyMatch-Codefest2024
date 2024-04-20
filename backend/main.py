aabac38fdbc6e8417ec3c125dc824d36ca4bec88
Davidarduinoclone <55816432+Davidarduinoclone@users.noreply.github.com>
2024-04-20 13:47:23 -0400

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,24 @@
 from flask import Flask
+from flask_migrate import Migrate
+#from flask_marshmallow import Marshmallow
+from flask_cors import CORS
+
+from audio_filter import *
+from genre_extract import *
+
 
 app = Flask(__name__)
-@app.route('/')
-def hello():
-    return "Hello, World!"
+
+audio_output = find_similar_tracks('5JEx7HbmvHQQswJCsoo9rA')
+genre_output = recommend_user_songs()
+
+@app.route('/audio')
+def audio():
+    return audio_output
+
+@app.route('/genre')
+def genre():
+    return genre_output
+
+if __name__ == '__main__':
+    app.run(debug=True)
